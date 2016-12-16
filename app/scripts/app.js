51e2326c0e8c638b6d150267c8f0a1a7c5e61060
lijiang <2761965683@qq.com>
2016-12-16 12:23:57 +0800

    李江

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -46,13 +46,13 @@ angular
      },function(){
 
      })
-     $scope.data=[{"name":"国际","english":"WORLD","title1":"环球视野","title2":"国际人物"},
-                    {"name":"财经","english":"FINANCE","title1":"股票","title2":"理财"}]
+     $scope.data={"name":"国际","english":"WORLD","title1":"环球视野","title2":"国际人物"}
+     $scope.data1={"name":"财经","english":"FINANCE","title1":"股票","title2":"理财"}
   }]).directive("autoplay",function(){
     return {
       restrict:"ECMA",
-      template:'<div><ul><li ng-repeat="x in data2">{{x.title}}</li></ul><div ng-repeat="a in data1" class="tu"><img src="{{a.img}}"/></div></div>',
-      scope:{data1:"=img",data2:"=title"},
+      template:'<div><div><span>{{data.name}}</span><span>{{data.english}}</span><span>{{data.title1}}</span><span>{{data.title2}}</span></div><ul><li ng-repeat="x in data2">{{x.title | center}}</li></ul><div ng-repeat="a in data1" class="tu"><img src="{{a.img}}"/></div></div>',
+      scope:{data1:"=img",data2:"=title",data:"=biao"},
       replace:true,
       link:function(s,e,a){
         e.find(".carousel").carousel({
@@ -62,4 +62,11 @@ angular
       }
 
     }
-  });
+  }).filter("center",function(){
+    return function(instr){
+      if(instr.length>=10){
+        return instr.slice(0,10)+"..."
+      }
+      
+    }
+  })
